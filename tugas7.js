f7a9bc67e6da77641b240adf7c4c72aab69d7477
User-niomic <user@niomic.com>
2021-06-26 01:50:39 +0700

    Tugas 7 Javascript
    Tugas ini kami kumpulkan untuk Niomic

--- tugas7.js
+++ tugas7.js
@@ -1,4 +1,7 @@
 var tinggi_badan=[140,143,144,141,157,160,162,164,165,170];
-for(let tinggi of tinggi_badan){
-  console.log(tinggi)
+for(var i=0;i<tinggi_badan.length;i++){
+  console.log(tinggi_badan[i])
+}
+for(let j of tinggi_badan){
+  console.log(j)
 }
